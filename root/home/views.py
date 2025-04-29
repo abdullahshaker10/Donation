dc7626ce1655263fa1c0ac3141ed5838ab5f7da8
abdulah shaker <abdullah.shaker2017@gmail.com>
2025-04-29 10:41:14 +0300

    add fromatting job

--- root/home/views.py
+++ root/home/views.py
@@ -1,10 +1,15 @@
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
+
+
+        
+
         context = super().get_context_data(**kwargs)
         # Example logic to fetch unread notifications
-        context['unread_notifications'] = 3  # Replace with dynamic logic
+        context["unread_notifications"] = 3  # Replace with dynamic logic
         return context
